68882ae4f675982170dbdaa2b918447f175aa582
Dharshini2409 <dharshu9327@gmail.com>
2025-04-04 18:39:56 +0530

    Create Animal1.java

---
+++ Abstraction/Animal1.java
@@ -0,0 +1,18 @@
+interface Animal {
+    void makeSound(); // Abstract method
+}
+
+// Implementing class
+class Cat implements Animal {
+    public void makeSound() {
+        System.out.println("Cat meows: Meow! Meow!");
+    }
+}
+
+// Main class
+public class InterfaceExample1 {
+    public static void main(String[] args) {
+        Cat myCat = new Cat();
+        myCat.makeSound();
+    }
+}
