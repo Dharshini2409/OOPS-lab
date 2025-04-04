0d1a00b90f77b610b16c0633e8f77da07883395d
Dharshini2409 <dharshu9327@gmail.com>
2025-04-04 19:04:48 +0530

    Create FileRead.java

---
+++ file handling/FileRead.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.Scanner;
+public class FileRead {
+    public static void main(String[] args) {
+        try {
+            File file = new File("example.txt");
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                String data = scanner.nextLine();
+                System.out.println(data);
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+}
