c798da7bd522285a7ff0a413cf3f03ab8c6f448d
Dharshini2409 <dharshu9327@gmail.com>
2025-04-05 18:00:03 +0530

    Create DateExample.java

---
+++ Packages/DateExample.java
@@ -0,0 +1,9 @@
+import java.time.LocalDate;
+
+public class DateExample {
+    public static void main(String[] args) {
+        LocalDate today = LocalDate.now();
+
+        System.out.println("Today's date is: " + today);
+    }
+}
